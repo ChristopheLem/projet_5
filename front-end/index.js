872db1a6878770c19bdad0db21b90e7272c76ddd
Christophe Lemercier <lemercier60@gmail.com>
2020-03-27 13:14:39 +0100

    crée fonction renderProduct

--- front-end/index.js
+++ front-end/index.js
@@ -1,32 +1,21 @@
 // URL de l'api
 const url = 'http://localhost:3000/api/teddies'
 
-// Affiche tous les produits
-const displayProducts = async () => {
-    const products = await getAllTeddies(url)
-    products.forEach(product => {
-    renderProduct(product.name, product._id, product.imageUrl, product.price);
-    });
-}
 // Récupère tous les ours en peluche
 const getAllTeddies = async (url) => {
     const response = await fetch(url);
     return await response.json();
 }
 
-const convertPrice = (number) => {
-    return (number / 1000).toString().replace('.', ',');
-}
 // Fourni l'affichage d'un produit 
 function renderProduct (productName, productId, productImg, productPrice) {
-    const products = document.querySelector('#products');   
+    const products = document.querySelector('#products');  // Récupère la div qui contiendra les différents articles 
     const article = document.createElement('article');
     article.innerHTML = `<img alt="${productName}" src="${productImg}">
     <button class="product-link" type="button"><a href="product/index.html?id=${productId}">Voir</a></button>
     <p class="product-title">${productName}</p>
-    <p class="price">${convertPrice(productPrice)}</p>
+    <p class="price">${productPrice / 1000}</p>
     `
     products.appendChild(article);     
 }
 
-displayProducts();
